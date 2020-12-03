e9055dc7349e50abfccdc465aafdd3e66095c918
Yura Ostrovsky <yurkasoft@gmail.com>
2020-12-04 01:42:11 +0200

    get data if no data

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,32 +1,5 @@
 import React, {Component} from 'react';
-//import './itemList.css';
-import styled from 'styled-components';
-
-const HeaderBlock = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 80px;
-`;
-
-const HeaderTitle = styled.h3`
-    font-size: 24px;
-    color: #fff;
-    margin: 0;
-`;
-
-const HeaderLinks = styled.ul`
-    display: flex;
-    margin: 0;
-    align-items: center;
-    color: #fff;
-    list-style-type: none;
-    li {
-        margin-right: 20px;
-        font-size: 18px;
-    }
-`;
-
+import './itemList.css';
 export default class ItemList extends Component {
 
     render() {
